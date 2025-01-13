585a235261f8e4bc2789e1b96e0f85ae3ca47e4f
Roni Seppälä <roni.o.seppala@gmail.com>
2025-01-13 09:08:01 +0200

    add MongoDB connection and update README for dotenv usage

---
+++ README.md
@@ -0,0 +1,3 @@
+Advanced Web Applications 2024 Week 7 Assignment
+
+dotenv is included in github because the way the assignment is returned, requires it to be in
--- app.ts
+++ app.ts
@@ -2,11 +2,17 @@ import express, { Express } from 'express';
 import path from "path"
 import router from "./src/routes/user"
 import dotenv from "dotenv"
+import mongoose, {Connection} from 'mongoose';
 
 dotenv.config()
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT as string) || 3000;
+
+const mongoDB: string = process.env.MONGODB_URI as string || 'mongodb://127.0.0.1:27017/testdb';
+mongoose.connect(mongoDB)
+mongoose.Promise = Promise
+const db: Connection = mongoose.connection
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
