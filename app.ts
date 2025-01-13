109c1c2509cfd34c82b3665c59044efe5bd00061
Roni Seppälä <roni.o.seppala@gmail.com>
2025-01-13 09:50:32 +0200

    implement user registration with password hashing

--- app.ts
+++ app.ts
@@ -3,17 +3,28 @@ import path from "path"
 import router from "./src/routes/user"
 import dotenv from "dotenv"
 import mongoose, {Connection} from 'mongoose';
+import morgan from 'morgan';
 
 dotenv.config()
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT as string) || 3000;
 
-const mongoDB: string = process.env.MONGODB_URI as string || 'mongodb://127.0.0.1:27017/testdb';
+const mongoDB: string = process.env.MONGODB_URI as string || 'mongodb://localhost:27017/testdb';
 mongoose.connect(mongoDB)
 mongoose.Promise = Promise
 const db: Connection = mongoose.connection
 
+
+db.on("error", console.error.bind(console, "MongoDB connection error"))
+
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/user', router);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+
+})
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,38 @@
 import { Router, Request, Response } from 'express';
+import { User, IUser } from '../models/User';
+import bcrypt from 'bcrypt';
 
 const router:Router = Router();
 
+router.post('/register', async (req: Request, res: Response) => {
+    try {
+        const existingUser: IUser | null = await User.findOne({email: req.body.email})
 
+        if (existingUser) {
+            res.status(403).send("Email already in use")
+            return
+        }
+
+        const salt: string = bcrypt.genSaltSync(10);
+        const hash: string = bcrypt.hashSync(req.body.password, salt);
+
+        const newUser: IUser = new User({
+            email: req.body.email,
+            password: hash,
+            username: req.body.username,
+            isAdmin: req.body.isAdmin
+        })
+
+        await newUser.save()
+
+        res.status(200).json(newUser)
+        return
+
+    } catch (error) {
+        console.error('Error in registration', error)
+        res.status(500).json({error: 'Internal server error'})
+        return
+    }
+});
 
 export default router
